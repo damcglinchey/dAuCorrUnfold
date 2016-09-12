27ebdc884e840606f7bf04d6800a2330b0554ea5
Darren McGlinchey <darren.mcglinchey@colorado.edu>
2016-09-12 15:30:50 -0600

    Add functions to get data and convert vn's to cn's so everything is in one place

--- unfold_input.py
+++ unfold_input.py
@@ -33,3 +33,69 @@ def corrdata(corr, file='correlations.root'):
     cor = np.vstack((x, y, e)).T
 
     return cor
+
+
+def getdata(energy, file='correlations.root'):
+    '''
+    Get data and also return list of labels
+    datalist:
+     [0]          := BBCS--FVTXS
+     [1:ui.npt+1] := CNT--BBCS in pT bins
+     [ui.npt+1:]  := CNT--FVTXS in pT bins
+    '''
+    print('\nGetting data from {}'.format(file))
+
+    # Get the BBCS -- FVTXS correlation for 0-5% central
+    datalist = [corrdata('dphi_corr_dAu{}_BBCSFVTXS_c0'.format(energy), file)]
+    labellist = ['BBCS--FVTXS 0-5%']
+
+    # Get the CNT -- BBCS correlation for 0-5% central pT bins
+    [datalist.append(corrdata('dphi_corr_dAu{}_CNTBBCS_c0_pt{}'.format(energy, i), file)) for i in range(npt)]
+    [labellist.append('CNT--BBCS 0-5% {:.2f}<pT<{:.2f}'.format(ptbins[i], ptbins[i+1])) for i in range(npt)]
+
+    # Get the CNT -- FVTXS correlation for 0-5% central pT bins
+    [datalist.append(corrdata('dphi_corr_dAu{}_CNTFVTXS_c0_pt{}'.format(energy, i), file)) for i in range(npt)]
+    [labellist.append('CNT--FVTXS 0-5% {:.2f}<pT<{:.2f}'.format(ptbins[i], ptbins[i+1])) for i in range(npt)]
+
+    print('  len(datalist) : {}'.format(len(datalist)))
+    print('  len(labellist): {}'.format(len(labellist)))
+
+    return datalist, labellist
+
+def vnini():
+    '''
+    Setup the initial vn guesses
+    vn_ini (and all parameter lists) nx3 matrix
+    [:, 0] := v_1
+    [:, 1] := v_2
+    [:, 2] := v_3
+    [0, :]           := v_n BBCS
+    [1, :]           := v_n FVTXS
+    [2:ui.ncpt+2, :] := v_n CNT in pT bins
+    '''
+    # vn_ini = np.vstack((np.full(2 + npt, 0.2),
+    # np.full(2 + npt, 0.1),
+    # np.full(2 + npt, 0.02))).T
+
+    csvi = 'dAu200/csv/4/pq.csv'
+    vn_ini = np.loadtxt(csvi, delimiter=',')[:, 0] # Previous step
+    vn_ini = vn_ini.reshape((3, 9)).T # Need to automate this ...
+
+    return vn_ini
+
+def calccn(vn, ncor):
+    '''
+    Calculate C_n's given the input vn matrix
+    '''
+    cn = np.zeros((ncor, vn.shape[1]))
+    cn[0, :] = vn[0, :] * vn[1, :]
+    cn[1:npt + 1, :] = vn[2:npt+2,:] * vn[0, :]
+    cn[npt + 1:, :] = vn[2:npt+2,:] * vn[1, :]
+
+    # This is a bit of a cheat, but force the C_1's to be negative
+    r = cn[:, 0] > 0
+    cn[r, 0] = -1 * cn[r, 0]
+
+    return cn
+
+
