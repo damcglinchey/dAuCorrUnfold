1732fe304ab846cbbe41b853c2b14e2d932bbfcd
Darren McGlinchey <darren.mcglinchey@colorado.edu>
2016-09-13 13:18:52 -0600

    use the new dimension variables in unfold_input

--- lnpmodels.py
+++ lnpmodels.py
@@ -55,81 +55,16 @@ def lncorr(data, cn):
     return mvn(data[:, 1:], cf.corr(data[:, 0], cn))
 
 
-def lndata(vn, data, xlim, nv, nn):
+def lndata(vn, data, xlim):
     '''
     Summed likelihood from the data
     '''
     if np.any(vn < xlim[:, 0]) or np.any(vn > xlim[:, 1]):
         return -np.inf
 
-    vn = vn.reshape((nn, nv)).T
+    vn = vn.reshape((ui.nn, ui.nv)).T
     cn = ui.calccn(vn, len(data))
     llsum = np.sum(np.array([lncorr(data[i], cn[i, :]) for i in range(len(data))]))
     return llsum
-
-
-
-
-
-# (Log) posterior pdf functions for MCMC samplers
-# Designed for linear systems Ax = b where
-# A = transfer matrix
-# x = vector of parameters to be found
-# b = vector of measured data points
-# Prior is p(x|prior pars)
-# Likelihood is p(b|A, x, prior pars)
-# Posterior is \propto prior*likelihood
-# -----------------------------------------------
-
-
-def logp_ept_dca(x, matlist, datalist, w, x_prior, alpha, xlim, L, scf, moddcalist):
-    '''
-    Intended for use with electron pt model (data) as first element of 
-    matlist (datalist), and electron DCA model/data as remaining elements.
-    '''
-    if np.any(x < xlim[:, 0]) or np.any(x > xlim[:, 1]):
-        return -np.inf
-
-    # Require the charm & bottom sum to stay the same
-    # c, b = ui.idx['c'], ui.idx['b']
-    # cf = np.absolute(np.sum(x[c]) - np.sum(x_prior[c])) / np.sum(x_prior[c])
-    # bf = np.absolute(np.sum(x[b]) - np.sum(x_prior[b])) / np.sum(x_prior[b])
-    # if cf > 0.01 or bf > 0.01:
-    #   return -np.inf
-
-    # lp_ept = w[0] * mvn(x, matlist[0], datalist[0])
-    lp_ept = w[0] * mvn_asymm(x, matlist[0], datalist[0])
-    # lp_dca = w[1] * dca_shape(x, matlist[1:], datalist[1:])
-    lp_dca = w[1] * dca_shape_mod(x, matlist[1:], datalist[1:], scf, moddcalist)
-    lp_reg = l2reg(x, x_prior, alpha, L)
-
-    # print(' lp_ept: {}'.format(lp_ept))
-    # print(' lp_dca: {}'.format(lp_dca))
-    # print(' lp_reg: {}'.format(lp_reg))
-    return lp_ept + lp_dca + lp_reg
-
-
-
-
-def mvn_asymm(x, A, d):
-    '''
-    Multivariate normal (Gaussian) log likelihood. 
-    x: trial solution
-    A: concatenated A_c, A_b matrices mapping x -> prediction
-    d: data,
-       column 0 contains points, 
-       column 1 contains upper errors. 
-       column 2 contains lower errors. 
-    Currently assumes data points are independent (diagonal covariance).
-    '''
-    c, b = ui.idx['c'], ui.idx['b']
-    p = np.dot(A[:, c], x[c]) + np.dot(A[:, b], x[b])
-    n = len(d[:, 0])
-    err = np.array([d[i,1] if p[i] >= d[i,0] else d[i,2] for i in range(n)])
-    # icov_data = np.diag(1. / (err ** 2))
-    icov_data = np.diag(np.full(n, 1.))
-    return lngauss(d[:, 0] / err, p / err, icov_data)
-
-
 
 
--- unfold.py
+++ unfold.py
@@ -8,10 +8,10 @@ import corr_funcs as cf
 import lnpmodels as lnp
 
 
-def unfold(step=3,
-           energy=20,
+def unfold(step=5,
+           energy=200,
            rootfile='correlations.root',
-           outdir='dAu20',
+           outdir='CNTBBCSFVTXS/dAu200',
            nwalkers=500,
            nburnin=500,
            nsteps=500):
@@ -61,22 +61,9 @@ def unfold(step=3,
     else:
         csvi = '{}/csv/{}/pq.csv'.format(outdir, step)
         vn_ini = np.loadtxt(csvi, delimiter=',')[:, 0] # Previous step
-        vn_ini = vn_ini.reshape((3, 9)).T # Need to automate this ...
     print(vn_ini.shape)
     print(vn_ini)
 
-    # Calculate the Cn's from initial vn's
-    cn_ini = ui.calccn(vn_ini, len(datalist))
-    print(cn_ini.shape)
-    print(cn_ini)
-
-
-    nv = vn_ini.shape[0]
-    nn = vn_ini.shape[1]
-
-    vn_ini = vn_ini.T.flatten()
-    ndim = len(vn_ini)
-    print('ndim = {}'.format(ndim))
 
 
 
@@ -85,16 +72,16 @@ def unfold(step=3,
     #--------------------------------------------------------------------------
 
     # Set parameter limits - put in array with shape (ndim,2)
-    parlimits = np.vstack((np.full(ndim, -1), np.full(ndim, 1))).T
+    parlimits = np.vstack((np.full(ui.ndim, -1), np.full(ui.ndim, 1))).T
 
     
-    # Ensemble of starting points for the walkers - shape (nwalkers, ndim)
-    print("Initializing {} {}-dim walkers...".format(nwalkers, ndim))
-    x0 = vn_ini * (1 + 0.1 * np.random.randn(nwalkers, ndim))
+    # Ensemble of starting points for the walkers - shape (nwalkers, ui.ndim)
+    print("Initializing {} {}-dim walkers...".format(nwalkers, ui.ndim))
+    x0 = vn_ini * (1 + 0.1 * np.random.randn(nwalkers, ui.ndim))
 
 
     # testing
-    llsum = lnp.lndata(vn_ini, datalist, parlimits, nv, nn)
+    llsum = lnp.lndata(vn_ini, datalist, parlimits)
     print('llsum = {}'.format(llsum))
 
     # Function returning values \propto posterior probability and arg tuple
@@ -102,9 +89,9 @@ def unfold(step=3,
     
     print("Setting up sampler...")
     fcn = lnp.lndata
-    args = (datalist, parlimits, nv, nn)
+    args = (datalist, parlimits)
     
-    sampler = emcee.EnsembleSampler(nwalkers, ndim, fcn, args=args, threads=2)
+    sampler = emcee.EnsembleSampler(nwalkers, ui.ndim, fcn, args=args, threads=2)
     
     print("Burning in for {} steps...".format(nburnin))
     pos, prob, state = sampler.run_mcmc(x0, nburnin)
@@ -114,11 +101,11 @@ def unfold(step=3,
     acc_frac = np.mean(sampler.acceptance_fraction)
     print("Mean acceptance fraction: {0:.3f}".format(acc_frac))
     
-    # Initial shape of sampler.chain is (nwalkers, nsteps, ndim).
-    # Reshape to (nwalkers*nsteps, ndim).
-    # Posterior quantiles: list of ndim (16,50,84) percentile tuples
+    # Initial shape of sampler.chain is (nwalkers, nsteps, ui.ndim).
+    # Reshape to (nwalkers*nsteps, ui.ndim).
+    # Posterior quantiles: list of ui.ndim (16,50,84) percentile tuples
     print("posterior quantiles")
-    samples = sampler.chain.reshape((-1, ndim))
+    samples = sampler.chain.reshape((-1, ui.ndim))
     pq = map(lambda v: (v[1], v[2] - v[1], v[1] - v[0]),
              zip(*np.percentile(samples, [16, 50, 84], axis=0)))
     pq = np.array(pq)
@@ -127,10 +114,10 @@ def unfold(step=3,
     # # Calculate the sample covariance matrix
     # # https://en.wikipedia.org/wiki/Sample_mean_and_sample_covariance
     # print("Calculating the covariance matrix")
-    # cov = np.zeros([ndim, ndim])
+    # cov = np.zeros([ui.ndim, ui.ndim])
     # N = samples.shape[0]
-    # for j in range(ndim):
-    #     for k in range(ndim):
+    # for j in range(ui.ndim):
+    #     for k in range(ui.ndim):
     #         cov[j, k] = 1./(N-1) * np.sum((samples[:, j] - pq[j, 0]) * (samples[:, k] - pq[k, 0]))
 
     # print(cov[0:7, 0:7])
@@ -146,7 +133,7 @@ def unfold(step=3,
     #--------------------------------------------------------------------------
 
     # calculate cn's
-    vn_final = pq[:, 0].reshape((nn, nv)).T
+    vn_final = pq[:, 0].reshape((ui.nn, ui.nv)).T
     cn_final = ui.calccn(vn_final, len(datalist))
     ll_final = [lnp.lncorr(datalist[i], cn_final[i, :]) for i in range(len(datalist))]
     fcorr_final = []
